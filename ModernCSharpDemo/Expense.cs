0b5eaf0dd3edb6f06f7aa17a8948150db565984a
Chrissie Zimmer <czimmer12@aol.com>
2026-08-27 11:56:49 -0400

    Records

---
+++ ModernCSharpDemo/Expense.cs
@@ -0,0 +1,1 @@
+public record Expense(string Title, double Amount, string Category);
---
+++ ModernCSharpDemo/Program.cs
@@ -0,0 +1,8 @@
+﻿Expense lunchExpense = new("Lunch", 250, "Food");
+
+Expense internetExpense = new("Internet", 999, "Utilities");
+
+Console.WriteLine("Expense Details");
+Console.WriteLine(lunchExpense);
+Console.WriteLine();
+Console.WriteLine(internetExpense);
