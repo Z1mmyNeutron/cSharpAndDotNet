62c0eb206835f026bbab90864b85e742e28ae3fd
Chrissie Zimmer <czimmer12@aol.com>
2026-08-27 13:48:41 -0400

    records

--- ModernCSharpDemo/Program.cs
+++ ModernCSharpDemo/Program.cs
@@ -1,8 +1,37 @@
 ﻿Expense lunchExpense = new("Lunch", 250, "Food");
+Expense updatedExpense = lunchExpense with{Amount = 300};
 
 Expense internetExpense = new("Internet", 999, "Utilities");
+
+object expense = updatedExpense;
+if(expense is Expense currentExpense){
+    Console.WriteLine();
+    Console.WriteLine("Pattern Match using is ");
+    Console.WriteLine($"{currentExpense.Title} - {currentExpense.Amount}");
+}
+
+string expenseCategory = updatedExpense.Amount switch{
+    < 500 => "Low Expense",
+    >= 500 and < 1000 => "Medium Expense",
+    _=> "High Expense"
+};
+
 
 Console.WriteLine("Expense Details");
 Console.WriteLine(lunchExpense);
 Console.WriteLine();
 Console.WriteLine(internetExpense);
+Console.WriteLine();
+Console.WriteLine(updatedExpense);
+Console.WriteLine();
+Console.WriteLine($"Expense Category: {expenseCategory}");
+
+string? notes = null;
+
+Console.WriteLine();
+Console.WriteLine("Expense Notes");
+if(notes is not null){
+    Console.WriteLine(notes);
+}else{
+    Console.WriteLine("No notes available");
+}
--- ModernCSharpDemo/obj/Debug/net10.0/ModernCSharpDemo.AssemblyInfo.cs
+++ ModernCSharpDemo/obj/Debug/net10.0/ModernCSharpDemo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ModernCSharpDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6466df309ea6e45bc569fe31dc9caa1f50b671d0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b5eaf0dd3edb6f06f7aa17a8948150db565984a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ModernCSharpDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ModernCSharpDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
