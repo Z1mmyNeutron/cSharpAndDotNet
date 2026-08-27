d6d7036f4ecda5e421b8cef6f707a219b5f7c846
Chrissie Zimmer <czimmer12@aol.com>
2026-08-26 20:46:19 -0400

    Interfaces and action lambdas

--- interfaceExample/Program.cs
+++ interfaceExample/Program.cs
@@ -11,12 +11,37 @@
 // printableExpense.PrintDetails();
 
 
-// List<Expense> expenses = new List<Expense>();
-// expenses.Add(new Expense("Lunch", 250, "Food"));
-// expenses.Add(new Expense("Taxi", 450, "Travel"));
-// expenses.Add(new Expense("Internet Bill", 999, "Utilities"));
+List<Expense> expenses = new List<Expense>();
+expenses.Add(new Expense("Lunch", 250, "Food"));
+expenses.Add(new Expense("Taxi", 450, "Travel"));
+expenses.Add(new Expense("Internet Bill", 999, "Utilities"));
 
 // Console.WriteLine("Expense List: ");
 // foreach(Expense expense in expenses){
 //     Console.WriteLine($"{expense.Title} - {expense.Amount} - {expense.Category}");
 // }
+
+
+// Dictionary<string, string> categoryDescription = new Dictionary<string, string>();
+// categoryDescription.Add("Food", "Daily Meals");
+// categoryDescription.Add("Travel", "Transportation expenses");
+// categoryDescription.Add("Utilities", "Monthly household bills");
+// Console.WriteLine();
+// Console.WriteLine(categoryDescription["Travel"]);
+// Console.WriteLine(categoryDescription["Food"]);
+// Console.WriteLine(categoryDescription["Utilities"]);
+
+Func<Expense, bool> isHighValueExpense = expense => expense.Amount > 500;
+Console.WriteLine();
+Console.WriteLine(isHighValueExpense(expenses[0]));
+Console.WriteLine(isHighValueExpense(expenses[2]));
+
+Action<Expense> displayExpense = expense => {
+    Console.WriteLine($"{expense.Title} - {expense.Amount}");
+
+};
+Console.WriteLine();
+foreach(Expense expense in expenses){
+    displayExpense(expense);
+}
+
--- interfaceExample/obj/Debug/net10.0/interfaceExample.AssemblyInfo.cs
+++ interfaceExample/obj/Debug/net10.0/interfaceExample.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("interfaceExample")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9967b0b462a66aa06e684b03162067665abf369")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2c3cdcda18b6dcea571d8055a8a1dfbacd86c6f")]
 [assembly: System.Reflection.AssemblyProductAttribute("interfaceExample")]
 [assembly: System.Reflection.AssemblyTitleAttribute("interfaceExample")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
